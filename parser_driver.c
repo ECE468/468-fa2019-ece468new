1e2ae37fa75415a772e97a95a15983e5e19072bf
Quang Nguyen <baoquang98@gmail.com>
2019-08-28 19:15:55 -0400

    Step 1 first finished, added test cases

--- parser_driver.c
+++ parser_driver.c
@@ -3,9 +3,12 @@
 
 extern int yylex();
 extern FILE * yyin;
+
+/*Doing in C because C++ don't have lots of example with flex/lex, we can also just drop this file and move the logic to the parser.l file*/
 int main(int argc, char ** argv)
 {
 	FILE * input;
+	//If supplied with 1 input argument, use it as input file name, otherwise, read from stdin
 	if (argc > 1) {
 		input = fopen(argv[1], "r");
 		if (input == NULL) {
@@ -15,11 +18,14 @@ int main(int argc, char ** argv)
 	} else {
 		input = stdin;
 	}
+	
+	//Read the file workd by word until the file is empty
 	yyin = input;
 	int count = yylex();
 	while (count) {
 		count = yylex();
 	}
-	(void) count;
+	(void) count; //this is to stop the compiler from complaining count is unused
+	
 	return (EXIT_SUCCESS);
 }
