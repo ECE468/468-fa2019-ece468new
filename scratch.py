118a7cad09fdec1f81a21eb367ca5a3f37db14f0
Ray <Ray@pal-nat186-91-179.itap.purdue.edu>
2019-09-23 18:41:19 -0400

    more scratch.py changes

--- scratch.py
+++ scratch.py
@@ -2,16 +2,15 @@ import os
 #import numpy as np
 import subprocess as sub
 def main():
-    #test = 1
-    #iter = 10
     inputs = [1,5,6,7,8,11,13,14,16,18,19,20,21,22]
-    results = []
+    #results = inputs
     for input in inputs:
-        sub.call(['./runme', 'input_test/Step2/test'+str(input)+'.micro', 'outputtest'])
+        sub.call(['./runme', 'input_test/Step3/test'+str(input)+'.micro', 'outputtest'])
+	sub.call(['diff', 'output_test/Step3/test'+str(input)+'.out', 'outputtest'])
         #path = os.path.join()
-        with open('outputtest') as file:
-            line = file.readline()
-            results.append(line)
-    print(results)
-
+        #with open('outputtest') as file:
+        #    line = file.readline()
+        #    results.append(line)
+    #print(results)
+	
 if __name__ == "__main__": main()
